4ee3fe40965fd8f094d7812faa90093d07421957
Sacha924 <24448@student.dorset-college.ie>
2023-10-28 11:13:36 +0200

    change thumb style

--- next_app/next-rainbowkit/src/components/Client/GetAllVotes.tsx
+++ next_app/next-rainbowkit/src/components/Client/GetAllVotes.tsx
@@ -42,10 +42,10 @@ export default function GetAllVotes() {
         return (
             <div className="relative p-4 m-2 border border-gray-300 rounded bg-gray-100 flex flex-col items-start min-w-[450px]" key={index}>
                 <div className="absolute top-2 right-2 flex space-x-2">
-                    <img width="48" height="48" src="https://img.icons8.com/emoji/48/thumbs-up-medium-skin-tone.png" alt="thumbs-up-medium-skin-tone" onClick={() => write({
+                    <img width="48" height="48" src="https://img.icons8.com/ios-glyphs/30/thumb-up--v1.png" alt="thumbs-up-medium-skin-tone" onClick={() => write({
                         args: [data ? data[0][index] : BigInt(0), true],
                     })} />
-                    <img width="48" height="48" src="https://img.icons8.com/emoji/48/thumbs-down-medium-dark-skin-tone.png" alt="thumbs-down-medium-dark-skin-tone" onClick={() => write({
+                    <img width="48" height="48" src="https://img.icons8.com/ios-glyphs/30/thumbs-down.png" alt="thumbs-down-medium-dark-skin-tone" onClick={() => write({
                         args: [data ? data[0][index] : BigInt(0), false],
                     })} />
                 </div>
